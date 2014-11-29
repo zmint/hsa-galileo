5eab710674bcc61efe63d456b6171955f6a864ed
Patrick <zmint@gmx.net>
2014-11-29 20:24:38 +0100

    Update MappingEV3.java

--- src/mapping/MappingEV3.java
+++ src/mapping/MappingEV3.java
@@ -1,9 +1,12 @@
+package mapping;
+
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.lwjgl.LWJGLException;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
 import static org.lwjgl.opengl.GL11.*;
+
 
 public class MappingEV3 {
 	public static void main(String[] args) {
