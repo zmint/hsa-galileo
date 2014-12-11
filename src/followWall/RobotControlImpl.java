eaa9f448cfb74c6976f29f30d1ca694e92c90b68
zmint <zmint@gmx.net>
2014-12-11 14:25:33 +0100

    fixed untracked files

--- src/followWall/RobotControlImpl.java
+++ src/followWall/RobotControlImpl.java
@@ -1,12 +1,12 @@
-package followWall;
-
-public class RobotControlImpl implements RobotControl {
-
-	public static final long MAX_PROGRAM_TIME = 80000;
-	
-	public void start (){
-		RobotController controller = new RobotController();
-
-		controller.start();
-	}
-}
+//package followWall;
+//
+//public class RobotControlImpl implements RobotControl {
+//
+//	public static final long MAX_PROGRAM_TIME = 80000;
+//	
+//	public void start (){
+//		RobotController controller = new RobotController();
+//
+//		controller.start();
+//	}
+//}
