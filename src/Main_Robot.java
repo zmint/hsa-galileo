a922b27ff1dd2ae41a439a587d6d9677d1ea44a9
zmint <zmint@gmx.net>
2014-12-12 14:59:36 +0100

    sample sensor data

--- src/Main_Robot.java
+++ src/Main_Robot.java
@@ -2,6 +2,7 @@
  * 
  */
 
+import followWall.RobotController;
 import lejos.hardware.Brick;
 import lejos.hardware.BrickFinder;
 import lejos.hardware.Key;
@@ -14,42 +15,58 @@ import util.Datatype;
 import util.UDP_Packet;
 
 public class Main_Robot {
+
+	public static void main(String[] args) {
+		sendSensorData();
+		// moveRobot();
+	}
+
+	private static void moveRobot() {
+		RobotController controller = new RobotController();
+		controller.start();
+	}
 	
-	public static void main(String[] args){
+	private static void sendSensorData() {
 		Client cl = new ClientImplUDP();
-		
+
 		// init robot
-	    Brick brick = BrickFinder.getDefault();
-	    //Port s1 = brick.getPort("S1");
-	    Port s2 = brick.getPort("S2");
-	    Port s3 = brick.getPort("S3");
-	    //Port s4 = brick.getPort("S4");
-	    
-	    //Ultrasonic Sensor
-		EV3UltrasonicSensor uss_back = new EV3UltrasonicSensor (s2);
-		SampleProvider sample_uss_back =  uss_back.getMode("Distance");
-		EV3UltrasonicSensor uss_front = new EV3UltrasonicSensor (s3);
-		SampleProvider sample_uss_front =  uss_front.getMode("Distance");
-	    
-	    float uss_front_sample [] = new float [1];
-	    float uss_back_sample [] = new float [1];
-	    
-	    Key escape = brick.getKey("Escape");
-	    
-	    while (!escape.isDown() ) {
-	    	sample_uss_back.fetchSample(uss_front_sample, 0);
-	    	sample_uss_front.fetchSample(uss_back_sample, 0);
-	    	
+		Brick brick = BrickFinder.getDefault();
+		// Port s1 = brick.getPort("S1");
+		Port s2 = brick.getPort("S2");
+		Port s3 = brick.getPort("S3");
+		Port s4 = brick.getPort("S4");
+
+		// Ultrasonic Sensor
+		EV3UltrasonicSensor uss_sback = new EV3UltrasonicSensor(s2);
+		SampleProvider sample_uss_sback = uss_sback.getMode("Distance");
+		EV3UltrasonicSensor uss_sfront = new EV3UltrasonicSensor(s3);
+		SampleProvider sample_uss_sfront = uss_sfront.getMode("Distance");
+		EV3UltrasonicSensor uss_front = new EV3UltrasonicSensor(s4);
+		SampleProvider sample_uss_front = uss_front.getMode("Distance");
+
+		float uss_sfront_sample[] = new float[1];
+		float uss_sback_sample[] = new float[1];
+		float uss_front_sample[] = new float[1];
+
+		Key escape = brick.getKey("Escape");
+
+		while (!escape.isDown()) {
+			sample_uss_sback.fetchSample(uss_sfront_sample, 0);
+			sample_uss_sfront.fetchSample(uss_sback_sample, 0);
+			sample_uss_front.fetchSample(uss_front_sample, 0);
+
 			// create a UDP packet with data
-			UDP_Packet packet = new UDP_Packet("EV2", Datatype.sens, 0, false,
-					sample_uss_back.toString());
-			UDP_Packet packet2 = new UDP_Packet("EV2", Datatype.sens, 0, false,
-					sample_uss_front.toString());
-			
+			for (int i = 0; i < uss_sback_sample.length; ++i) {
+
+			}
+			UDP_Packet packet = new UDP_Packet("EV2", Datatype.sens, 0, 
+					"uss_sback: " + uss_sback_sample[0] + " uss_fback: "
+							+ uss_sfront_sample[0] + " uss_front: " + uss_front_sample[0]);
+
+
 			// send the packet
 			cl.send(packet);
-			cl.send(packet2);
-	    }
-		
+		}
+
 	}
 }
